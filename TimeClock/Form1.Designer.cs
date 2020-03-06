c426240a5120c8435a5a0bb4da9f7b8f66e68bac
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2020-03-05 19:46:16 -0500

    Initial Commit
    - Initial Commit

--- TimeClock/Form1.Designer.cs
+++ TimeClock/Form1.Designer.cs
@@ -29,12 +29,128 @@
 								private void InitializeComponent()
 								{
 												this.components = new System.ComponentModel.Container();
+												this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+												this.label1 = new System.Windows.Forms.Label();
+												this.label2 = new System.Windows.Forms.Label();
+												this.txtDescription = new System.Windows.Forms.TextBox();
+												this.btnClockIn = new System.Windows.Forms.Button();
+												this.btnClockOut = new System.Windows.Forms.Button();
+												this.txtCurrentTime = new System.Windows.Forms.TextBox();
+												this.timer1 = new System.Windows.Forms.Timer(this.components);
+												this.lblClockIndicator = new System.Windows.Forms.Label();
+												this.SuspendLayout();
+												// 
+												// monthCalendar1
+												// 
+												this.monthCalendar1.Enabled = false;
+												this.monthCalendar1.Location = new System.Drawing.Point(15, 30);
+												this.monthCalendar1.Name = "monthCalendar1";
+												this.monthCalendar1.TabIndex = 0;
+												// 
+												// label1
+												// 
+												this.label1.AutoSize = true;
+												this.label1.Location = new System.Drawing.Point(12, 4);
+												this.label1.Name = "label1";
+												this.label1.Size = new System.Drawing.Size(142, 17);
+												this.label1.TabIndex = 1;
+												this.label1.Text = "Day/Time Information";
+												// 
+												// label2
+												// 
+												this.label2.AutoSize = true;
+												this.label2.Location = new System.Drawing.Point(336, 4);
+												this.label2.Name = "label2";
+												this.label2.Size = new System.Drawing.Size(116, 17);
+												this.label2.TabIndex = 3;
+												this.label2.Text = "Work Description";
+												// 
+												// txtDescription
+												// 
+												this.txtDescription.Font = new System.Drawing.Font("OCR A Extended", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+												this.txtDescription.Location = new System.Drawing.Point(339, 30);
+												this.txtDescription.Multiline = true;
+												this.txtDescription.Name = "txtDescription";
+												this.txtDescription.Size = new System.Drawing.Size(563, 241);
+												this.txtDescription.TabIndex = 4;
+												// 
+												// btnClockIn
+												// 
+												this.btnClockIn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+												this.btnClockIn.Location = new System.Drawing.Point(12, 349);
+												this.btnClockIn.Name = "btnClockIn";
+												this.btnClockIn.Size = new System.Drawing.Size(261, 79);
+												this.btnClockIn.TabIndex = 5;
+												this.btnClockIn.Text = "Clock In";
+												this.btnClockIn.UseVisualStyleBackColor = true;
+												this.btnClockIn.Click += new System.EventHandler(this.btnClockIn_Click);
+												// 
+												// btnClockOut
+												// 
+												this.btnClockOut.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+												this.btnClockOut.Location = new System.Drawing.Point(641, 349);
+												this.btnClockOut.Name = "btnClockOut";
+												this.btnClockOut.Size = new System.Drawing.Size(261, 79);
+												this.btnClockOut.TabIndex = 6;
+												this.btnClockOut.Text = "Clock Out";
+												this.btnClockOut.UseVisualStyleBackColor = true;
+												this.btnClockOut.Click += new System.EventHandler(this.btnClockOut_Click);
+												// 
+												// txtCurrentTime
+												// 
+												this.txtCurrentTime.Enabled = false;
+												this.txtCurrentTime.Location = new System.Drawing.Point(15, 249);
+												this.txtCurrentTime.Name = "txtCurrentTime";
+												this.txtCurrentTime.Size = new System.Drawing.Size(262, 22);
+												this.txtCurrentTime.TabIndex = 7;
+												// 
+												// timer1
+												// 
+												this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+												// 
+												// lblClockIndicator
+												// 
+												this.lblClockIndicator.AutoSize = true;
+												this.lblClockIndicator.BackColor = System.Drawing.Color.Red;
+												this.lblClockIndicator.Location = new System.Drawing.Point(381, 380);
+												this.lblClockIndicator.Name = "lblClockIndicator";
+												this.lblClockIndicator.Size = new System.Drawing.Size(152, 17);
+												this.lblClockIndicator.TabIndex = 8;
+												this.lblClockIndicator.Text = "                                    ";
+												// 
+												// Form1
+												// 
+												this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 												this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-												this.ClientSize = new System.Drawing.Size(800, 450);
+												this.ClientSize = new System.Drawing.Size(914, 450);
+												this.Controls.Add(this.lblClockIndicator);
+												this.Controls.Add(this.txtCurrentTime);
+												this.Controls.Add(this.btnClockOut);
+												this.Controls.Add(this.btnClockIn);
+												this.Controls.Add(this.txtDescription);
+												this.Controls.Add(this.label2);
+												this.Controls.Add(this.label1);
+												this.Controls.Add(this.monthCalendar1);
+												this.Name = "Form1";
 												this.Text = "Form1";
+												this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+												this.Load += new System.EventHandler(this.Form1_Load);
+												this.ResumeLayout(false);
+												this.PerformLayout();
+
 								}
 
 								#endregion
+
+								private System.Windows.Forms.MonthCalendar monthCalendar1;
+								private System.Windows.Forms.Label label1;
+								private System.Windows.Forms.Label label2;
+								private System.Windows.Forms.TextBox txtDescription;
+								private System.Windows.Forms.Button btnClockIn;
+								private System.Windows.Forms.Button btnClockOut;
+								private System.Windows.Forms.TextBox txtCurrentTime;
+								private System.Windows.Forms.Timer timer1;
+								private System.Windows.Forms.Label lblClockIndicator;
 				}
 }
 
--- TimeClock/Form1.cs
+++ TimeClock/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,86 @@ namespace TimeClock
 {
 				public partial class Form1 : Form
 				{
+
+								private DateTime startTime;
+
+								private DateTime endTime;
+
+								private bool onClock;
+
 								public Form1()
 								{
 												InitializeComponent();
+												monthCalendar1.SetDate(DateTime.Now);
+												txtCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss tt");
+								}
+
+								private void btnClockIn_Click(object sender, EventArgs e)
+								{
+												if (!onClock)
+												{
+																DialogResult result = MessageBox.Show("Are you sure you want to clock in?", "Clock In", MessageBoxButtons.YesNo);
+																if (result == DialogResult.Yes)
+																{
+																				startTime = DateTime.Now;
+																				onClock = true;
+																				lblClockIndicator.BackColor = Color.Green;
+																}
+												}
+								}
+
+								private void timer1_Tick(object sender, EventArgs e)
+								{
+												txtCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss tt");
+								}
+
+								private void Form1_Load(object sender, EventArgs e)
+								{
+												timer1.Start();
+								}
+
+								private void btnClockOut_Click(object sender, EventArgs e)
+								{
+												if (onClock)
+												{
+																DialogResult result = MessageBox.Show("Are you sure you want to clock out?", "Clock Out", MessageBoxButtons.YesNo);
+																if (result == DialogResult.Yes)
+																{
+																				endTime = DateTime.Now;
+																				onClock = false;
+																				if (!File.Exists("C:\\Users\\jacob\\Desktop\\TimeClock.txt"))
+																				{
+																								FileStream fs = File.Create("C:\\Users\\jacob\\Desktop\\TimeClock.txt");
+																								fs.Close();
+																				}
+																				int len = (startTime.ToString("dddd, dd MMMM yyyy") + " : " + startTime.ToString("HH:mm tt") + " :: " + endTime.ToString("HH:mm tt") + " ::: " + endTime.Subtract(startTime).Hours + "h-" + endTime.Subtract(startTime).Minutes + "m::: " + endTime.Subtract(startTime).TotalHours).Length;
+																				string lineBreak = "";
+																				for (int i = 0; i < len + 1; i++)
+																				{
+																								lineBreak += "=";
+																				}
+																				StreamWriter sw = File.AppendText("C:\\Users\\jacob\\Desktop\\TimeClock.txt");
+																				sw.WriteLine($"{lineBreak}\n{startTime.ToString("dddd, dd MMMM yyyy")} : {startTime.ToString("HH:mm tt")} :: {endTime.ToString("HH:mm tt")} ::: {endTime.Subtract(startTime).Hours}h-{endTime.Subtract(startTime).Minutes}m ::: {endTime.Subtract(startTime).TotalHours}\n{txtDescription.Text.ToString()}\n{lineBreak}\n");
+																				sw.Close();
+																				lblClockIndicator.BackColor = Color.Red;
+																}
+												}
+								}
+
+								private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+								{
+												if (onClock)
+												{
+																DialogResult result = MessageBox.Show("You are on the clock!\nAre you sure you want to clock out?", "Warning - On The Clock", MessageBoxButtons.YesNo);
+																if (result == DialogResult.Yes)
+																{
+																				e.Cancel = false;
+																}
+																else
+																{
+																				e.Cancel = true;
+																}
+												}
 								}
 				}
 }
